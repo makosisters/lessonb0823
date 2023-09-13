0dc99f41d04de9e7815fc7275f311cefcfd58ce3
machiko <machiko@example.com>
2023-09-13 10:48:03 +0000

    sample

---
+++ sample1_4.js
@@ -0,0 +1,10 @@
+let message;
+
+message = 'Hello';
+
+let number = 12;
+
+console.log(message);
+console.log(number);
+
+
---
+++ sample1_5.js
@@ -0,0 +1,7 @@
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed ${str}`;
+
+console.log('str =',str ); 
+console.log('str2 =', str2);
+console.log('phrase =', phrase);
