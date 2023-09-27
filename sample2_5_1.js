e9e9143c8e66972d9a00bd711e0f2a7f30c52ae4
machiko <machiko@example.com>
2023-09-27 10:52:31 +0000

    jsinfo 2.8

---
+++ sample2_5_1.js
@@ -0,0 +1,6 @@
+let n = 123;
+let str = "Hello";
+let strn = "123";
+let namefieldcheked = true;
+let agefieldcheked = false;
+let x;
---
+++ sample2_8_1.js
@@ -0,0 +1,17 @@
+let x = 1;
+
+x = -x;
+
+console.log(x);
+
+let s = "my" + "string";
+console.log(s); 
+
+console.log("1" + 2 );
+console.log( 2 + "1" );
+
+let counter = 2;
+
+counter--;
+
+console.log(counter);
