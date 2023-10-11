82e674ea41a199f7215e255feeded9ede5801e92
machiko <machiko@example.com>
2023-10-11 10:51:30 +0000

    29

---
+++ sample2_10.js
@@ -0,0 +1,10 @@
+let year = 2015;
+if (year == 201 ) {
+    console.log( 'You are right' );
+} else {
+    console.log( 'How can you be so wrong!' );
+}
+
+let age = 18;
+let accsessAllowed = ( age > 19 ) ? true : false;
+console.log( accsessAllowed );
---
+++ sample2_9_1.js
@@ -0,0 +1,8 @@
+console.log( 2 == 1 );
+
+let result = 5 > 4;
+console.log( result );
+
+console.log( 'Z' > 'A' );
+
+console.log( '2' > 1 );
